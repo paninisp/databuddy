b9ceeb92084a926a0c58bd85cf50e90cfa0ddb28
patapanp <patapanp@onid.oregonstate.edu>
2014-06-24 14:34:13 -0700

    update2

--- basemodule/connectDB.php
+++ basemodule/connectDB.php
@@ -1,8 +1,8 @@
 <?php
-	$dbhost = 'engr-db.engr.oregonstate.edu:3307';
-	$dbname = 'OSUDatabuddies';
-	$dbuser = 'OSUDatabuddies';
-	$dbpass = '';
+	$dbhost = 'mysql.cs.orst';
+	$dbname = 'OSU_Databuddies';
+	$dbuser = 'OSU_Databuddies';
+	$dbpass = 'e3dh7TpPvujsAxCn';
 
 	$mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
     or die("Error connecting to database server");
